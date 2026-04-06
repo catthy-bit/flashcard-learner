fdd6c813a93f4de49e24e26eedd878c6694ca062
catthy chu <catthychu@gmail.com>
2026-04-06 15:29:40 +1000

    AddFlashcard.jsx: Creates a card

--- frontend/AddFlashcard.jsx
+++ frontend/AddFlashcard.jsx
@@ -2,4 +2,21 @@
 import { useState } from 'react';
 import './Flashcard.css'; 
 
+// Function: Create card
+
+export default function AddFlashcard({addCard}) {
+    // Initialises states
+    const [question, setQuestion] = useState('');
+    const [answer, setAnswer] = useState('');
+
+    // Makes flashcards that contain question and answer
+    const makeFlashcard = () => {
+        if (question.trim() && answer.trim()) {
+            addCard(question, answer); //sent to App.jsx
+            setQuestion('');
+            setAnswer('');
+        }
+    }
+    
+}
 
