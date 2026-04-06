4e56a0d46b943a81c1a3b5547e36d8217ebfe051
catthy chu <catthychu@gmail.com>
2026-04-06 21:30:29 +1000

    Feature: Returns a series of flashcards with unique id, question and answers

---
+++ frontend/src/CardOverview.jsx
@@ -0,0 +1,21 @@
+
+import FlashCards from './Flashcard';
+import './App.css';
+
+// Function: View all Flashcards 
+
+export default function ViewCards({cards}) {
+
+    // same as for each c in cards, create a flashcard using card's data
+    // c passes individual question and answer
+    return (
+        <div>
+            {cards.map((c) => (
+                <FlashCards key={c.id} cards = {c}/>
+            ))}
+        </div>
+        
+    ) 
+}
+
+
