67c6c8de2144a4e0665d5a00b21e1f75c11d747a
catthy chu <catthychu@gmail.com>
2026-04-08 00:08:05 +1000

    Feature: Added States & Functions and, Updated Return to allow edits that can be saved or cancelled

--- frontend/src/Flashcard.jsx
+++ frontend/src/Flashcard.jsx
@@ -1,12 +1,14 @@
 
 import {useState} from 'react';
 
-//Function: Flip and dismiss card
-
-export default function FlashCards( { card }) {
+export default function FlashCards( { card, editedCard }) {
     
     const [isFlipped, setIsFlipped] = useState(false);
     const [isUsed, setIsUsed] = useState(false);
+
+    const [isEdited, setIsEdited] = useState(false);
+    const [editQuestion, setEditQuestion] = useState('');
+    const [editAnswer, setEditAnswer] = useState('');
 
     const flipCard = () => {
         setIsFlipped(true);
@@ -15,11 +17,57 @@ export default function FlashCards( { card }) {
     const useCard = () => {
         setIsUsed(true);
     }
+
+    const editCard = () => {
+        setIsEdited(true);
+        setEditQuestion(card.question);
+        setEditAnswer(card.answer);
+    }
+
+    const saveEditedCard = () => {
+        setIsEdited(false);
+        editedCard(editQuestion, editAnswer);
+    }
+
+    const cancelEditedCard = () => {
+        setIsEdited(false);
+    }
     
     return (
-        <div className={isUsed ? 'hide-card' : 'card'} onClick={flipCard}>
-            {isFlipped ? card.answer : card.question}
-            {isFlipped && <button onClick={useCard}>Done</button>}
+        // Role 1: If true, card input can be edited and saved or, edited and cancelled
+        // Role 2: Clicking on the card flips its and shows question
+        <div>
+            <div className={isEdited ? 'edit-input' : 'leave-input'}>
+                {isEdited && 
+                    <input 
+                        value={editQuestion}
+                        onChange = {(e) => setEditQuestion (e.target.value)}
+                    />
+                }
+
+                {isEdited && 
+                    <input 
+                        value={editAnswer}
+                        onChange = {(e) => setEditAnswer (e.target.value)}
+                    />
+                }
+
+                {isEdited && 
+                    <div>
+                        <button onClick={saveEditedCard}>Save</button>
+                        <button onClick={cancelEditedCard}>Cancel</button>
+                    </div>
+                }
+            </div> 
+
+            <button onClick={editCard}>Edit Card</button>
+
+            
+            <div className={isUsed ? 'hide-card' : 'card'} onClick={flipCard}>
+                {isFlipped ? card.answer : card.question}
+                {isFlipped && <button onClick={useCard}>Done</button>}
+            </div>
+            
         </div>
 
     )
