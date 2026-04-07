b0053f0d050f2d45ec2ed9b28019395732f1969a
catthy chu <catthychu@gmail.com>
2026-04-08 08:28:00 +1000

    Feature: Added removeFlashCard function to find deleted Card and remove from array. Added function when all cards are viewable

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,10 @@ import {useState} from 'react';
 import AddFlashcard from './AddFlashcard';
 import ViewCards from './CardOverview';
 import './App.css';
+import FlashCards from './Flashcard';
 
 export default function App() {
-
+  // creates an array for all flashcards
   const [allCards, setAllCards] = useState([]);
 
   //for switching between viewing all cards and adding new card to deck
@@ -13,6 +14,7 @@ export default function App() {
   //if isSwitched == true -> View all
   //if isSwitched == false -> Add new flashcard
 
+  // Takes all cards part of the card list and 
   const handleFlashCard = (question, answer) => {
     setAllCards([...allCards, {
       id: Date.now(),
@@ -20,13 +22,25 @@ export default function App() {
       answer: answer,
     }]);
   }
+  
+  const removeFlashCard = (id) => {
+    //
+    const remainingCards = allCards.filter(card => card.id != id);
+    setAllCards(remainingCards);
+  }
 
   return (
     <div>
-      <button onClick={() => setIsSwitched(true)}>View All</button>
-      <button onClick={() => setIsSwitched(false)}>Add New Card</button>
+      {/* Buttons' states are read when specific button is clicked */}
+      <button className="view-all-btn" onClick={() => setIsSwitched(true)}>View All</button>
+      <button className="add-new-card-btn" onClick={() => setIsSwitched(false)}>Add New Card</button>
+      
+      {/* If switched is true, view all flashcards. Else, show a add new flashcard button 
+          Should delete a card from entire collection when removeFlashCard is used
+      */}
+      
       <div className={isSwitched ? 'card-overview' : 'add-new-flashcard'}>
-        {isSwitched ? <ViewCards cards={allCards}></ViewCards> : <AddFlashcard addCard={handleFlashCard}></AddFlashcard>}
+        {isSwitched ? <ViewCards cards={allCards} deletedCard={removeFlashCard}></ViewCards> : <AddFlashcard addCard={handleFlashCard}></AddFlashcard>}
       </div>
     </div>
   ) 
