b046da881f2b1f3ea30d1b0f7df833c0f9f32050
catthy chu <catthychu@gmail.com>
2026-04-08 16:04:11 +1000

    Feature: Adds Function - Gets connection to  create a data table if it doesn't exist. Executes data table when app starts and creates the app which passes through the load

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,31 @@
 import sqlite3 
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
 
 def get_database():
     return sqlite3.connect('flashcards.db')
 
+def create_flashcards_table():
+    # Establishes the connection to get appropriate database
+    conn = get_database()
+
+    # Use cursor to interact with data
+    cursor = conn.cursor()
+
+    # Create a data table containing the following columns if it doesnt exist
+    cursor.execute("""CREATE TABLE IF NOT EXISTS flashcards (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        question TEXT NOT NULL,
+                        answer TEXT NOT NULL
+                   )""")
+    conn.commit()
+    conn.close()
+
+@asynccontextmanager
+async def load(app):
+    create_flashcards_table()
+    yield
+
+app = FastAPI(lifespan=load)
 
